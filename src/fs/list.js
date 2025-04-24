e1a392adca17585ae80266d5c21f55147595928e
eklap <ekalape@libero.it>
2025-04-24 17:26:34 +0200

    feat: implement functions inside fs section

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { opendir } from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const folderPath = path.join(dirname, "files");
+
+  try {
+    const filesDir = await opendir(folderPath);
+
+    for await (const file of filesDir) {
+      console.log("\x1b[36m%s\x1b[0m", file.name);
+    }
+  } catch (e) {
+    console.error("\x1b[31m%s\x1b[0m", "FS operation failed");
+  }
 };
 
 await list();
