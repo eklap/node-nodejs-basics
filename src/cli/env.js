9299a377991ad23505ab19362732def440eedd09
eklap <ekalape@libero.it>
2025-04-25 13:47:28 +0200

    feat: implement functions inside cli section

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { argv } from "process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const data = argv.slice(2);
+  data.forEach((item) => {
+    const [key, value] = item.split("=");
+    if (key.startsWith("RSS_")) {
+      console.log("\x1b[32m%s\x1b[0m", `${key}=${value}`);
+    }
+  });
 };
 
 parseEnv();
