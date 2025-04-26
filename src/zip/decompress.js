e8c23e2e6caddabb3948215a6b7eaa26dadd6bbd
eklap <ekalape@libero.it>
2025-04-26 19:30:22 +0200

    fear: implement fns inside zip section

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createUnzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { unlink } from "fs/promises";
+
 const decompress = async () => {
-    // Write your code here 
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const sourceFile = path.join(dirname, "files", "archive.gz");
+  const destinationFile = path.join(dirname, "files", "fileToCompress.txt");
+
+  const unzip = createUnzip();
+  const source = createReadStream(sourceFile);
+  const destination = createWriteStream(destinationFile);
+  try {
+    await pipeline(source, unzip, destination);
+    await unlink(sourceFile);
+  } catch (e) {
+    console.error("\x1b[31m%s\x1b[0m", "FS operation failed");
+  }
 };
 
 await decompress();
