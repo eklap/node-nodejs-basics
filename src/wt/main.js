4f55905e5a2cf596d21945d245784d4f101e3e10
eklap <ekalape@libero.it>
2025-04-27 18:48:14 +0200

    feat: implement fn inside wt section

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { cpus } from "os";
+import { Worker, isMainThread } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpslength = cpus().length;
+
+  const array = Array.from({ length: cpslength }, (_, i) => i + 10);
+
+  if (isMainThread) {
+    const order = [];
+
+    const workerTask = async (n) =>
+      new Promise((resolve, reject) => {
+        const worker = new Worker(new URL("./worker.js", import.meta.url), {
+          workerData: { nData: n, workerNumber: n - 9 },
+        });
+
+        worker.on("message", (result) => {
+          if (result.type === "ready") {
+            order.push(result.workerNumber);
+          }
+          if (result.type === "result") {
+            resolve({ status: "resolved", data: result.data, workerNumber: result.workerNumber });
+          }
+        });
+        worker.on("error", (error) => {
+          reject({ status: "error", data: null, workerNumber: n - 9 });
+        });
+      });
+
+    const results = (await Promise.allSettled(array.map((n) => workerTask(n)))).map(
+      (v) => v.value ?? v.reason,
+    );
+
+    const finalResult = order.map((wn) => {
+      const res = results.find((r) => r.workerNumber === wn);
+      return {
+        status: res.status,
+        data: res.data ?? null,
+      };
+    });
+    console.log("finalResult", finalResult);
+  } else {
+  }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
+import { parentPort, workerData } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const nData = workerData.nData;
+  const wn = workerData.workerNumber;
+  parentPort.postMessage({ type: "ready", workerNumber: wn });
+
+  if (wn === 10) {
+    throw new Error("Error");
+  }
+
+  parentPort.postMessage({ type: "result", data: nthFibonacci(nData), workerNumber: wn });
 };
 
 sendResult();
