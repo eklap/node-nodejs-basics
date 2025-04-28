f7fe7c5b49bc3859bacc852946a33eaaa9e180b1
eklap <ekalape@libero.it>
2025-04-28 19:26:03 +0200

    feat: implement fn and change a little script inside cp section

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -3,10 +3,20 @@ const args = process.argv.slice(2);
 console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
+process.on("message", (message) => {
+  if (message === "START") {
+    process.send("Child process started");
+  }
+  if (message === "CLOSE") {
+    process.send("Child process closed");
+    process.exit(0);
+  }
+});
+
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  const chunkStringified = chunk.toString();
+  if (chunkStringified.includes("CLOSE")) process.exit(0);
+  process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
 };
 
-process.stdin.on('data', echoInput);
+process.stdin.on("data", echoInput);
